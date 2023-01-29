a6088c92e0563c32a2b891daec882995b27e12ea
artelightstudios <artelightstudios@gmail.com>
2023-01-29 17:23:59 +0900

    WebView 2023 01 29

--- splash_screen/splash/lib/main.dart
+++ splash_screen/splash/lib/main.dart
@@ -1,32 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:splash/screen/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('AppBar Title'),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.orange,
-          ),
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/logo.png',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
